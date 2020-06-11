7f80dcb9f44379b804e2dfe1b9854800ed6c03a8
lijie36 <lijie36@baidu.com>
2020-06-11 13:49:09 +0800

    feature: do something

--- src/init.ts
+++ src/init.ts
@@ -1,24 +1,40 @@
 import { fetchData } from './fetch-data';
+import {
+    saveDataSync,
+    hasNewData,
+    htmlProcess,
+    getSrcDataFromHtml
+} from './data-process';
+import fs from 'fs';
+import path from 'path';
 // import { writeFileSync, readFileSync } from 'fs';
 // import { resolve } from 'path';
 
-interface ProcessOptions{}
+interface ProcessOptions { }
 
 export async function init(options?: ProcessOptions) {
     let htmlContent = await fetchData();
-    
-    // start your code here
-    console.log(htmlContent);
+    const existencePath = path.resolve('./data/existence.data');
+    const existenceData = fs.readFileSync(existencePath, 'utf-8').split('\n');
 
-    if (false) {
-        getcha();
+    const datas = getSrcDataFromHtml(htmlContent);
+    const originData = existenceData.length ? existenceData : datas;
+
+    fs.writeFileSync(existencePath, originData.join('\n'));
+    console.log(datas, originData, existenceData);
+    
+    const hasNew = hasNewData(datas, originData);
+    if (hasNew && hasNew.length > 0) {
+        getcha(hasNew);
     } else {
         console.log('未能找到！');
     }
 }
 
-function getcha() {
+function getcha(data) {
+    const outputPath = path.resolve('./data/output.data');
     console.log('发现了新增内容！');
+    saveDataSync(data, outputPath);
 }
 
 // 以下是读写文件示例，__dirname指的是当前文件的工作目录
